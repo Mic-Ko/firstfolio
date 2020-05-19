45ed9a75a910b685c634dc1e8c6fd8dd87fc8787
Mic-Ko <kozibor@gmail.com>
2020-05-19 14:38:48 +0100

    add video to hobby part

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
 AOS.init({
     duration: 1200,
   })
+  var button = document.getElementById('toggle-button');
+  button.addEventListener('click', function() {
+    var el = document.getElementById('video-bg');
+    el.style.display = (el.style.display != 'none' ? 'none' : 'block' );
+  });
