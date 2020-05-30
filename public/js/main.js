5203f0e08795f8db16c0622df700cd0fbe03fc14
Mic-Ko <kozibor@gmail.com>
2020-05-31 00:12:24 +0100

    work with responsive cdn

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,11 @@ button.addEventListener('click', function() {
     var el = document.getElementById('video-bg');
     el.style.display = (el.style.display != 'none' ? 'none' : 'block' );
 });
+
+
+var ident = document.getElementById("div1")
+var button = document.getElementById('toggle-button');
+button.addEventListener('click', function() {
+    var info = document.getElementById('info')
+    info.classList.toggle("active")
+  })
