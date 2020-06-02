96aefde51f6885641ece515c2884e93cccc1ba97
Mic-Ko <kozibor@gmail.com>
2020-06-02 01:54:06 +0100

    add new skill icon responive test

--- public/js/main.js
+++ public/js/main.js
@@ -15,3 +15,37 @@ button.addEventListener('click', function() {
     var info = document.getElementById('info')
     info.classList.toggle("active")
   })
+
+  var button = document.getElementById('toggle-button');
+  button.addEventListener('click', function() {
+      var vidbutton = document.querySelector('#toggle-button')
+      vidbutton.classList.toggle("vidbutton")
+    })
+
+
+$('.button').first().addClass('show');
+
+$('.button').click(function(){
+  var $this = $(this);
+  $siblings = $this.parent().children(),
+  position = $siblings.index($this);
+
+  $('.content div').removeClass('show').eq(position).addClass('show');
+
+  $siblings.removeClass('show');
+  $this.addClass('show');
+})
+
+
+$('.button').first().addClass('show');
+
+$('.button').mouseover(function(){
+  var $this = $(this);
+  $siblings = $this.parent().children(),
+  position = $siblings.index($this);
+
+  $('.content div').removeClass('show').eq(position).addClass('show');
+
+  $siblings.removeClass('show');
+  $this.addClass('show');
+})
